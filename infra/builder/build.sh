0984ef5f7cdd3558d07b0adc2b2f923cc33044f3
taldy <taldykin.egor@gmail.com>
2018-11-29 18:03:54 +0200

    cd #10

--- infra/builder/build.sh
+++ infra/builder/build.sh
@@ -7,9 +7,4 @@ yarn install
 yarn run check
 yarn run build
 
-cd dist/
-ls -l
-
-cd ..
-
-firebase deploy --only hosting --token 1/BEHNF6mlLWzcqw46yzqP76IVsJVNw_3Yo3HL04XY9d4
+firebase deploy --only hosting --token 1/PX_r7_X2tXqTr8U_GlKmbDrA4QYoYrewdxM_csAq-L4
