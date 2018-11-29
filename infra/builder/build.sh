e88b6621864beee7cd322c15133754c73446dd3d
taldy <taldykin.egor@gmail.com>
2018-11-29 17:45:33 +0200

    cd #8

--- infra/builder/build.sh
+++ infra/builder/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -xe
 cd /app
+
+yarn global add firebase-tools
 yarn install
+
 yarn run check
 yarn run build
+
+firebase deploy --only hosting --token 1/BEHNF6mlLWzcqw46yzqP76IVsJVNw_3Yo3HL04XY9d4
