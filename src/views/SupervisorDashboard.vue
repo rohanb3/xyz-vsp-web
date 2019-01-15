0a5f1c25334d331391535c8b87a61f7679598312
more-v-kaple <morevkaple@gmail.com>
2019-01-15 22:17:27 +0200

    done bar chart

--- src/views/SupervisorDashboard.vue
+++ src/views/SupervisorDashboard.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="supervisor-dashboard-page">
     <semicircle-chart :datasets="datasets"/>
+    <rating-chart :datasets="datasetsForRatingChart" title="Team Client Rating" route="/"/>
   </div>
 </template>
 
 <script>
 import SemicircleChart from '@/components/charts/SemicircleChart/SemicircleChart';
+import RatingChart from '@/components/RatingChart';
 
 export default {
   name: 'SupervisorDashboard',
   components: {
     SemicircleChart,
+    RatingChart,
   },
   data() {
     return {
+      datasetsForRatingChart: [{title: 5, value: 2303},
+        {title: 4, value: 700},
+        {title: 3, value: 138},
+        {title: 2, value: 38},
+        {title: 1, value: 35}],
       // Now Payload with time
       datasets: [
         { color: '#7ed321', value: 150, title: 'result', type: 'time' },
