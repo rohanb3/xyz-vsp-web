a1cbc805096fdf94a21e600e6b4367f692d601ce
Unknown <anton.loktev@ardas.biz>
2019-02-08 22:10:34 +0800

    fix be bug with deploy

--- functions/fake-be/routes/operatorDetails.js
+++ functions/fake-be/routes/operatorDetails.js
@@ -1,8 +1,7 @@
 const operatorDetails = require('../fixtures/operatorDetails.json');
 
 function getOperatorDetails(req, res) {
-  const { operator } = req;
-  res.send({ ...operatorDetails });
+  res.send(operatorDetails);
 }
 
 exports.getOperatorDetails = getOperatorDetails;
