2f02942039fd6688ae6bbc3d5956044cb4504f0b
Dmitry <d.drehval@gmail.com>
2019-03-26 13:52:09 +0200

    intermediet commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <v-app id="app">
     <router-view/>
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      class="blur-svg"
+    >
+      <defs>
+        <filter id="blur-filter">
+          <feGaussianBlur stdDeviation="3"></feGaussianBlur>
+        </filter>
+      </defs>
+    </svg>
   </v-app>
 </template>
 
--- src/containers/IncomingCallPopup.vue
+++ src/containers/IncomingCallPopup.vue
@@ -1,11 +1,15 @@
 <template>
-  <v-layout row justify-center>
-    <v-dialog v-model="isDialogShown" persistent>
+  <v-layout row justify-center v-cssBlurOverlay v-if="isDialogShown">
+    <v-dialog :content-class="'incoming-call-popup'" v-model="isDialogShown" persistent>
       <div class="main" :style="{backgroundImage: backgroundImage}">
         <div class="reject-call">
           <v-icon color="white" class="icon-reject" @click="ignoreCall">call_end</v-icon>
         </div>
-        <div class="blurred-area"/>
+        <div class="call-from-company-name">
+          <span>{{$t('incoming.call.popup')}}</span>
+          <br>
+          <span>{{brandName}}</span>
+        </div>
         <v-btn class="accept-call" @click="acceptCall">
           <v-icon class="icon-accept">call</v-icon>
           <p>{{$t('pick.up')}}</p>
@@ -21,10 +25,15 @@
 
 <script>
 import moment from 'moment';
+import cssBlurOverlay from '@/directives/cssBlurOverlay';
+
 import { initializeOperator, acceptCall, disconnectOperator } from '@/services/call';
 
 export default {
   name: 'IncomingCallPopup',
+  directives: {
+    cssBlurOverlay,
+  },
   data() {
     return {
       dialogMinimizedByUser: false,
@@ -43,13 +52,17 @@ export default {
         .hour(0)
         .minute(0)
         .second(this.counter)
-        .format('mm : ss');
+        .format('mm:ss');
     },
     isIncomingCall() {
       return this.$store.getters.isIncomingCall;
     },
     isDialogShown() {
-      return !this.dialogMinimizedByUser && this.isIncomingCall;
+      // return !this.dialogMinimizedByUser && this.isIncomingCall;
+      return true;
+    },
+    brandName() {
+      return `${this.$t('incoming.call.popup.brand.from')} «` + `Walmart` + `»`;
     },
   },
   mounted() {
@@ -75,12 +88,20 @@ export default {
 };
 </script>
 
+<style lang="scss">
+.incoming-call-popup {
+  border-radius: 10.5px;
+  background-color: rgba(57, 143, 251, 0.7);
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
+}
+</style>
+
 <style scoped lang="scss">
 @import '~@/assets/styles/variables.scss';
 
 .main {
   width: 311px;
-  height: 484px;
+  height: 264px;
   border-radius: 10px;
   display: flex;
   flex-direction: column;
@@ -95,11 +116,21 @@ export default {
     justify-content: flex-end;
     .icon-reject {
       margin: 10px;
-      width: 30px;
-      height: 30px;
+      width: 25px;
+      height: 25px;
+      font-size: 16px;
       background-color: $call-dialogue-reject-icon-background-color;
       border-radius: 30px;
     }
+  }
+
+  .call-from-company-name {
+    width: 100%;
+    position: absolute;
+    top: 52px;
+    font-size: 24px;
+    color: #ffffff;
+    text-align: center;
   }
 
   .accept-call {
@@ -115,17 +146,16 @@ export default {
     bottom: 48px;
     left: 36px;
     z-index: 100;
+    text-transform: none;
+    font-weight: normal;
+    box-shadow: none;
 
     .icon-accept {
-      margin-right: 40px;
+      font-size: 32px;
+      margin-right: 30px;
     }
   }
 
-  .blurred-area {
-    height: 74px;
-    filter: blur(13.9px);
-    background: $call-dialogue-blurred-background;
-  }
   .incoming-call {
     position: absolute;
     bottom: 16px;
