47c1694f17783bb07526a06d1d8a764f6541b38c
Ekateryna Yakymenko <morevkaple@gmail.com>
2019-03-28 17:27:42 +0200

    comment service-worker functionality

--- vue.config.js
+++ vue.config.js
@@ -16,12 +16,12 @@ module.exports = {
       enableInSFC: false,
     },
   },
-  pwa: {
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      swSrc: 'src/service-worker.js',
-    },
-  },
+  // pwa: {
+  //   workboxPluginMode: 'InjectManifest',
+  //   workboxOptions: {
+  //     swSrc: 'src/service-worker.js',
+  //   },
+  // },
   devServer: {
     proxy: {
       '/api/v1': {
