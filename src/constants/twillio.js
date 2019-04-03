ca3211cd37e2a5c5fae00db72a6524b5e38b87eb
Ekateryna Yakymenko <morevkaple@gmail.com>
2019-04-03 15:02:18 +0300

    fix extension for shating screen name

--- src/constants/twillio.js
+++ src/constants/twillio.js
@@ -1,3 +1,3 @@
 // eslint-disable-next-line
 export const EXTENSION_ID = process.env.EXTENSION_ID || 'bkhgjdfacdkkffgmjfndalaeelhjaiha';
-export const EXTENSION_URL = `https://chrome.google.com/webstore/detail/test-video-capture-app/${EXTENSION_ID}`;
+export const EXTENSION_URL = `https://chrome.google.com/webstore/detail/xyzies-vsp-screensharing/${EXTENSION_ID}`;
