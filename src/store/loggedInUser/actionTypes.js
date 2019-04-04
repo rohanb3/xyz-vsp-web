0c23e967d742e465935838493c477702b906de60
Dmitry <d.drehval@gmail.com>
2019-04-04 13:37:17 +0300

    implemented url avatar

--- src/store/loggedInUser/actionTypes.js
+++ src/store/loggedInUser/actionTypes.js
@@ -2,3 +2,4 @@ export const GET_PROFILE_DATA = 'GET_PROFILE_DATA';
 export const CHANGE_PROFILE_DATA = 'CHANGE_PROFILE_DATA';
 export const LOGIN = 'LOGIN';
 export const REFRESH_TOKEN = 'REFRESH_TOKEN';
+export const GET_PHOTO = 'GET_PHOTO';
