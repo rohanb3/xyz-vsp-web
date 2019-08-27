25bd9fee03cb8a1ed002d8518cbdb0a6f38b2e22
Vlad <vladislavmoiseenkov@gmail.com>
2019-08-27 18:12:14 +0300

    change config

--- vue.config.js
+++ vue.config.js
@@ -19,10 +19,19 @@ module.exports = {
       enableInSFC: false,
     },
   },
+  // pwa: {
+  //   workboxPluginMode: 'InjectManifest',
+  //   workboxOptions: {
+  //     swSrc: 'src/service-worker.js',
+  //   },
+  // },
   pwa: {
+    // configure the workbox plugin
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
-      swSrc: 'src/service-worker.js',
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/registerServiceWorker.js',
+      // ...other Workbox options...
     },
   },
   devServer: {
