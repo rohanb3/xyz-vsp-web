45973faab74b4e7fd41b13fe03544733fe4904b0
Vlad <vladislavmoiseenkov@gmail.com>
2019-12-05 15:06:05 +0200

    fix after review

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -97,44 +97,49 @@ export default {
   methods: {
     async submit() {
       const { email, password } = this;
-      if (this.$refs.form.validate() && this.agreement && !this.loading) {
+      if (this.validate()) {
         this.loading = true;
         try {
           await this.$store.dispatch(LOGIN, { email, password });
           await this.$store.dispatch(GET_PROFILE_DATA);
           this.$router.push({ path: '/dashboard' });
         } catch (e) {
-          const {
-            response: { status, data },
-          } = e;
-
-          const translations = {
-            'Tenant is not specified for your Company. Please, contact support':
-              'tenant.not.specified',
-          };
-
-          const title = translations[data] || data;
-
-          switch (status) {
-            case RESPONSE_STATUSES.FORBIDDEN:
-              this.$notify({
-                group: 'notifications',
-                title: this.$t(title),
-                type: 'error',
-              });
-              break;
-            default:
-              this.$notify({
-                group: 'notifications',
-                title: 'Login failed',
-                type: 'error',
-              });
-              break;
-          }
+          this.onError(e);
         } finally {
           this.loading = false;
         }
       }
+    },
+    onError(e) {
+      const {
+        response: { status, data },
+      } = e;
+
+      const translations = {
+        'Tenant is not specified for your Company. Please, contact support': 'tenant.not.specified',
+      };
+
+      const title = translations[data] || data;
+
+      switch (status) {
+        case RESPONSE_STATUSES.FORBIDDEN:
+          this.$notify({
+            group: 'notifications',
+            title: this.$t(title),
+            type: 'error',
+          });
+          break;
+        default:
+          this.$notify({
+            group: 'notifications',
+            title: 'Login failed',
+            type: 'error',
+          });
+          break;
+      }
+    },
+    validate() {
+      return this.$refs.form.validate() && this.agreement && !this.loading;
     },
   },
 };
