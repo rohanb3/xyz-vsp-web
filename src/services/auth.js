1494cfab79deef8398d5bd4e457d46e1e90b1f12
Andrey Matushenko <andrii.matiushenko@ardas.dp.ua>
2020-02-28 18:33:58 +0200

    Reuse USER_LOGOUT action in auth service

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,7 @@
 import store from '@/store';
 import { disconnect as disconnectVSPSocket } from '@/services/vspSocket/transport';
 
-import { REMOVE_TOKEN, CLEAR_PROFILE_DATA } from '@/store/loggedInUser/mutationTypes';
+import { USER_LOGOUT } from '@/store/loggedInUser/actionTypes';
 
 import api from './api';
 
@@ -9,9 +9,8 @@ export function login(userName, password) {
   return api.post('/login', { userName, password }).then(response => response.data);
 }
 
-export const logout = () => {
-  store.commit(REMOVE_TOKEN);
-  store.commit(CLEAR_PROFILE_DATA);
+export const logout = async () => {
+  await store.dispatch(USER_LOGOUT);
 
   disconnectVSPSocket();
 };
